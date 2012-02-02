ed75d1eb40c7caff32f4d44e56a59560a1490d3b
Jude Mwenda <judemwenda@gmail.com>
2012-02-02 06:57:05 -0500

    added the proper hander to solve the topics issues. git blame jmwenda

--- src/elimu/masomo/models.py
+++ src/elimu/masomo/models.py
@@ -79,7 +79,7 @@ class Topic(models.Model):
 	chapter = models.ForeignKey(Chapter)
         title = models.CharField("title", max_length=25)
         def __unicode__(self):
-		return self.chapter
+		return self.title
         @models.permalink
         def get_absolute_url(self):
                 return ('topics',(),{})
